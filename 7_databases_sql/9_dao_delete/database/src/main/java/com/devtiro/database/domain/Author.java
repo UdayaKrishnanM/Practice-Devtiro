6587100b142834553220d19d4f6fb6846b6d64d5
UdayaKrishnanM <muk031101@gmail.com>
2024-04-29 12:17:31 +0530

    added

---
+++ 7_databases_sql/9_dao_delete/database/src/main/java/com/devtiro/database/domain/Author.java
@@ -0,0 +1,46 @@
+package com.devtiro.database.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Author {
+
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+    
+    
+}
---
+++ 7_databases_sql/9_dao_delete/database/src/main/java/com/devtiro/database/domain/Book.java
@@ -0,0 +1,46 @@
+package com.devtiro.database.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+
+    private String isbn;
+
+    private String title;
+
+    private Long authorId;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Long getAuthorId() {
+		return authorId;
+	}
+
+	public void setAuthorId(Long authorId) {
+		this.authorId = authorId;
+	}
+
+    
+    
+}
